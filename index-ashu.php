59f0cb4d040b03ef217044676daebe41677d8057
Ashutoah Biswal <aaronstone201299@gmail.com>
2022-05-22 20:12:31 +0530

    vbn

--- index-ashu.php
+++ index-ashu.php
@@ -1,4 +1,4 @@
-<h1> this index file comes from my container image<br> test 4</h1>
+<h1> this index file comes from my container image<br> test 7</h1>
 
 <br>text from container, inside pod, inside k8s cluster"
 <br>
